fa39a7bbb9f0116c240b60ae10576d09b1c948d3
Artem <artemzhdanov1299@gmail.com>
2023-03-26 23:15:18 +0300

    Add project files.

---
+++ 23.03hm/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._03hm
+{
+    interface IClone
+    {
+        IClone Clone();
+    }
+
+    class Transport : IClone
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public string skorost { get; set; }
+        public Transport() { }
+        public Transport(string name, string type, string speed)
+        {
+            this.name = name;
+            this.type = type;
+            this.skorost = speed;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Type: {type}");
+            Console.WriteLine($"Speed: {skorost}");
+        }
+        public IClone Clone()
+        {
+            Transport trans = new Transport();
+            trans.name = name;
+            trans.type = type;
+            trans.skorost = skorost;
+            return trans;
+        }
+    }
+    class Ship : Transport, IClone { }
+    class Auto : Transport, IClone { }
+    class Plane : Transport, IClone { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ship ship = new Ship();
+            ship.name = "Авианосец";
+            ship.type = "Военный";
+            ship.speed = "Средний";
+
+            ship.Clone();
+
+        }
+    }
+}
