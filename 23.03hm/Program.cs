dd15ec94fd21274386bc7a6dbe5c85c5aeaa3f37
Artem <artemzhdanov1299@gmail.com>
2023-03-26 23:19:58 +0300

    .

--- 23.03hm/Program.cs
+++ 23.03hm/Program.cs
@@ -13,45 +13,53 @@ namespace _23._03hm
 
     class Transport : IClone
     {
-        public string name { get; set; }
-        public string type { get; set; }
+        public string nazvanie { get; set; }
+        public string vid_transpotra { get; set; }
         public string skorost { get; set; }
         public Transport() { }
         public Transport(string name, string type, string speed)
         {
-            this.name = name;
-            this.type = type;
-            this.skorost = speed;
+            this.nazvanie = nazvanie;
+            this.vid_transpotra = vid_transpotra;
+            this.skorost = skorost;
         }
         public void Print()
         {
-            Console.WriteLine($"Name: {name}");
-            Console.WriteLine($"Type: {type}");
+            Console.WriteLine($"Name: {nazvanie}");
+            Console.WriteLine($"Type: {vid_transpotra}");
             Console.WriteLine($"Speed: {skorost}");
         }
         public IClone Clone()
         {
             Transport trans = new Transport();
-            trans.name = name;
-            trans.type = type;
+            trans.nazvanie = nazvanie;
+            trans.vid_transpotra = vid_transpotra;
             trans.skorost = skorost;
             return trans;
         }
     }
-    class Ship : Transport, IClone { }
-    class Auto : Transport, IClone { }
-    class Plane : Transport, IClone { }
+    class Corabl : Transport, IClone { }
+    class Mashina : Transport, IClone { }
+    class Samolet : Transport, IClone { }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Ship ship = new Ship();
-            ship.name = "Авианосец";
-            ship.type = "Военный";
-            ship.speed = "Средний";
-
+            Corabl ship = new Corabl();
+            ship.nazvanie = "Круизный";
+            ship.vid_transpotra = "Турестический";
+            ship.skorost = "Высокая";
             ship.Clone();
-
+            Mashina car = new Mashina();
+            car.nazvanie = "ferari";
+            car.vid_transpotra = "спортивная";
+            car.skorost = "Очень Высокая";
+            car.Clone();
+            Samolet plane = new Samolet();
+            ship.nazvanie = "Истребитель";
+            ship.vid_transpotra = "Военная";
+            ship.skorost = "Очень Высокая";
+            ship.Clone();
         }
     }
 }
